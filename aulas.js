3287b17e74ce6877ae1078acf6cc2785d1341d10
higorlopespro <90470606+higorlopespro@users.noreply.github.com>
2021-09-11 12:42:25 -0300

    Add files via upload

---
+++ aulas.js
@@ -0,0 +1,26 @@
+function aula01(){
+    var nova = window.document.getElementById("video")
+    var titulo = window.document.getElementById("titulo")
+    var desc = window.document.getElementById("desc")
+    titulo.innerText = ("Aula 01 - Introdução ao design")
+    nova.innerHTML = ("<iframe width='560' height='315' src='https://www.youtube.com/embed/hdDHg1p3YVc' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>")
+    desc.innerText = ("Você está assistindo a aula de número 01")
+}
+
+function aula02(){
+    var nova = window.document.getElementById("video")
+    var titulo = window.document.getElementById("titulo")
+    var desc = window.document.getElementById("desc")
+    titulo.innerText = ("Aula 02 - Entendendo um pouco mais")
+    nova.innerHTML = ("<iframe width='560' height='315' src='https://www.youtube.com/embed/PB254Cfjlyk' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>")
+    desc.innerText = ("Você está assistindo a aula de número 02")
+}
+
+function aula03(){
+    var nova = window.document.getElementById("video")
+    var titulo = window.document.getElementById("titulo")
+    var desc = window.document.getElementById("desc")
+    titulo.innerText = ("Aula 03 - Dando seus primeiros passos")
+    nova.innerHTML = ("<iframe width='560' height='315' src='https://www.youtube.com/embed/PB254Cfjlyk' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>")
+    desc.innerText = ("Você está assistindo a aula de número 0")
+}
